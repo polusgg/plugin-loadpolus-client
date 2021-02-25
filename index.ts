986ad64b3c080b22a4abc04f5652c9ed50e19457
Cody Scott <cs475x@icloud.com>
2021-02-24 18:01:48 -0800

    Cleanup, and update game state in redis on game.started and game.ended events

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
-import { BasePlugin } from "../../../lib/api/plugin";
-import { Server } from "../../../lib/server";
+import { GameState, Language, Level } from "nodepolus/lib/types/enums";
+import { LobbyInstance } from "nodepolus/lib/api/lobby";
+import { BasePlugin } from "nodepolus/lib/api/plugin";
+import { Server } from "nodepolus/lib/server";
 import Redis from "ioredis";
-import { GameState, Language, Level } from "../../../lib/types/enums";
-import { LobbyInstance } from "../../../lib/api/lobby";
 
 declare const server: Server;
 
+type Config = {
+  name: string;
+};
+
 export default class extends BasePlugin {
   private readonly redis: Redis.Redis;
-  private readonly config: Record<any, any>;
-  /*private readonly gamemodeReservedCodes: Map<string, string> = new Map([
-    ["[]00", "foo"],
-    ["[]02", "bar"],
-  ]);*/
+  private readonly config: Config;
 
   constructor() {
     super(server, {
       name: "LoadPolus",
-      version: [4, 2, 0],
+      version: [1, 0, 0],
     });
 
     this.redis = new Redis({
@@ -25,24 +25,24 @@ export default class extends BasePlugin {
       host: "127.0.0.1",
     });
 
-    // public ip and port used by this node
     this.config = {
-      "name": "local",
-    }
+      name: "local",
+    };
 
     server.on("server.lobby.created", event => {
       const lobby = event.getLobby();
+      const options = lobby.getOptions();
 
       this.redis.hmset(`loadpolus.lobby.${lobby.getCode()}`, {
-        "host": server.getDefaultLobbyAddress(),
-        "port": server.getDefaultLobbyPort(),
-        "level": Level[lobby.getOptions().getLevels()[0]],
-        "impostorCount": lobby.getOptions().getImpostorCount(),
-        "language": Language[lobby.getOptions().getLanguages()[0]],
-        "currentPlayers": 0,
-        "maxPlayers": lobby.getOptions().getMaxPlayers(),
-        "gameState": GameState[lobby.getGameState()],
-        "gamemode": lobby.getMeta<string>("gamemode"),
+        host: server.getDefaultLobbyAddress(),
+        port: server.getDefaultLobbyPort(),
+        level: Level[options.getLevels()[0]],
+        impostorCount: options.getImpostorCount(),
+        language: Language[options.getLanguages()[0]],
+        currentPlayers: 0,
+        maxPlayers: options.getMaxPlayers(),
+        gameState: GameState[lobby.getGameState()],
+        gamemode: lobby.getMeta<string>("gamemode"),
         "public": lobby.isPublic() ? "true" : "false",
       });
 
@@ -50,79 +50,41 @@ export default class extends BasePlugin {
     });
 
     server.on("server.lobby.destroyed", event => {
-      const lobby = event.getLobby();
-      
-      this.redis.del(`loadpolus.lobby.${lobby.getCode()}`);
-      this.redis.srem(`loadpolus.node.${this.config.name}.lobbies`, lobby.getCode());
+      const code = event.getLobby().getCode();
+
+      this.redis.del(`loadpolus.lobby.${code}`);
+      this.redis.srem(`loadpolus.node.${this.config.name}.lobbies`, code);
     });
 
-    server.on("player.joined", event => {
-      const lobby = event.getLobby();
-      this.updateCurrentPlayers(lobby);
-    });
-
-    server.on("player.left", event => {
-      const lobby = event.getLobby();
-      this.updateCurrentPlayers(lobby);
-    });
-
-    server.on("player.kicked", event => {
-      const lobby = event.getLobby();
-      this.updateCurrentPlayers(lobby);
-    });
-
-    server.on("player.banned", event => {
-      const lobby = event.getLobby();
-      this.updateCurrentPlayers(lobby);
-    });
-
+    server.on("player.joined", event => this.updateCurrentPlayers(event.getLobby()));
+    server.on("player.left", event => this.updateCurrentPlayers(event.getLobby()));
+    server.on("player.kicked", event => this.updateCurrentPlayers(event.getLobby()));
+    server.on("player.banned", event => this.updateCurrentPlayers(event.getLobby()));
     server.on("server.lobby.list", event => event.cancel());
+    server.on("game.started", event => this.updateGameState(event.getGame().getLobby()));
+    server.on("game.ended", event => this.updateGameState(event.getGame().getLobby()));
 
     server.on("lobby.privacy.updated", event => {
-      const newValue = event.isPublic() ? "true" : "false";
-
       this.redis.hmset(`loadpolus.lobby.${event.getLobby().getCode()}`, {
-        "public": newValue,
-      })
-    })
+        "public": event.isPublic() ? "true" : "false",
+      });
+    });
   }
 
   private updateCurrentPlayers(lobby: LobbyInstance): void {
     this.redis.hmset(`loadpolus.lobby.${lobby.getCode()}`, {
-      "currentPlayers": lobby.getPlayers().length,
-      "currentConnections": lobby.getConnections().length,
+      currentPlayers: lobby.getPlayers().length,
+      currentConnections: lobby.getConnections().length,
     });
 
     this.redis.hmset(`loadpolus.node.${this.config.name}`, {
-      "currentConnections": server.getConnections().size,
+      currentConnections: server.getConnections().size,
     });
   }
 
-  /*private matchmakingFindLobby(targetGamemode: string) {
-    const allGames = server.getLobbies();
-    const lobbyCandidates: Lobby[];
-
-    for (let i = 0; i < allGames.length; i++) {
-      const currentGame = allGames[i];
-      const gamemode = currentGame.getMeta("gamemode");
-
-      if (!currentGame.isPublic()) {
-        continue;
-      }
-
-      if (currentGame.getGameState() != GameState.NotStarted) {
-        continue;
-      }
-      
-      if (gamemode != targetGamemode) {
-        continue;
-      }
-
-      if (currentGame.getPlayers().length >= currentGame.getSettings().getMaxPlayers()) {
-        continue;
-      }
-
-      if (currentGame.get)
-    }
-  }*/
+  private updateGameState(lobby: LobbyInstance): void {
+    this.redis.hmset(`loadpolus.lobby.${lobby.getCode()}`, {
+      gameState: GameState[lobby.getGameState()],
+    });
+  }
 }
