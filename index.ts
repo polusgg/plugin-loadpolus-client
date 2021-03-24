40ae93d6fde373e335e007d549d11a566b8b3f8d
cybershard <ashwatdd01@gmail.com>
2021-03-24 10:54:34 -0400

    Update nodepolus, and use @nodepolus/nodepolus package

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
-import { GameState, Language, Level } from "nodepolus/lib/types/enums";
-import { LobbyInstance } from "nodepolus/lib/api/lobby";
-import { BasePlugin } from "nodepolus/lib/api/plugin";
-import { Server } from "nodepolus/lib/server";
+import { GameState, Language, Level } from "@nodepolus/nodepolus/lib/types/enums";
+import { LobbyInstance } from "@nodepolus/nodepolus/lib/api/lobby";
+import { BasePlugin } from "@nodepolus/nodepolus/lib/api/plugin";
 import Redis from "ioredis";
-
-declare const server: Server;
 
 type Config = {
   name: string;
@@ -15,7 +12,7 @@ export default class extends BasePlugin {
   private readonly config: Config;
 
   constructor() {
-    super(server, {
+    super({
       name: "LoadPolus",
       version: [1, 0, 0],
     });
@@ -29,13 +26,13 @@ export default class extends BasePlugin {
       name: "local",
     };
 
-    server.on("server.lobby.created", event => {
+    this.server.on("server.lobby.created", event => {
       const lobby = event.getLobby();
       const options = lobby.getOptions();
 
       this.redis.hmset(`loadpolus.lobby.${lobby.getCode()}`, {
-        host: server.getDefaultLobbyAddress(),
-        port: server.getDefaultLobbyPort(),
+        host: this.server.getDefaultLobbyAddress(),
+        port: this.server.getDefaultLobbyPort(),
         level: Level[options.getLevels()[0]],
         impostorCount: options.getImpostorCount(),
         language: Language[options.getLanguages()[0]],
@@ -49,22 +46,22 @@ export default class extends BasePlugin {
       this.redis.sadd(`loadpolus.node.${this.config.name}.lobbies`, lobby.getCode());
     });
 
-    server.on("server.lobby.destroyed", event => {
+    this.server.on("server.lobby.destroyed", event => {
       const code = event.getLobby().getCode();
 
       this.redis.del(`loadpolus.lobby.${code}`);
       this.redis.srem(`loadpolus.node.${this.config.name}.lobbies`, code);
     });
 
-    server.on("player.joined", event => this.updateCurrentPlayers(event.getLobby()));
-    server.on("player.left", event => this.updateCurrentPlayers(event.getLobby()));
-    server.on("player.kicked", event => this.updateCurrentPlayers(event.getLobby()));
-    server.on("player.banned", event => this.updateCurrentPlayers(event.getLobby()));
-    server.on("server.lobby.list", event => event.cancel());
-    server.on("game.started", event => this.updateGameState(event.getGame().getLobby()));
-    server.on("game.ended", event => this.updateGameState(event.getGame().getLobby()));
+    this.server.on("player.joined", event => this.updateCurrentPlayers(event.getLobby()));
+    this.server.on("player.left", event => this.updateCurrentPlayers(event.getLobby()));
+    this.server.on("player.kicked", event => this.updateCurrentPlayers(event.getLobby()));
+    this.server.on("player.banned", event => this.updateCurrentPlayers(event.getLobby()));
+    this.server.on("server.lobby.list", event => event.cancel());
+    this.server.on("game.started", event => this.updateGameState(event.getGame().getLobby()));
+    this.server.on("game.ended", event => this.updateGameState(event.getGame().getLobby()));
 
-    server.on("lobby.privacy.updated", event => {
+    this.server.on("lobby.privacy.updated", event => {
       this.redis.hmset(`loadpolus.lobby.${event.getLobby().getCode()}`, {
         "public": event.isPublic() ? "true" : "false",
       });
@@ -78,7 +75,7 @@ export default class extends BasePlugin {
     });
 
     this.redis.hmset(`loadpolus.node.${this.config.name}`, {
-      currentConnections: server.getConnections().size,
+      currentConnections: this.server.getConnections().size,
     });
   }
 
